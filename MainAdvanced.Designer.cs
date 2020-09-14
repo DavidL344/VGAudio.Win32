c334126b1adf6978dfb7afb04be081ab137a4f07
DavidL344 <69001314+DavidL344@users.noreply.github.com>
2020-09-14 21:00:23 +0200

    Add more advanced conversion options for HCA

--- MainAdvanced.Designer.cs
+++ MainAdvanced.Designer.cs
@@ -31,9 +31,18 @@
             this.lst_brstm_audioFormat = new System.Windows.Forms.ComboBox();
             this.lbl_brstm_audioFormat = new System.Windows.Forms.Label();
             this.lbl_options = new System.Windows.Forms.Label();
-            this.lbl_hca_audioQuality = new System.Windows.Forms.Label();
-            this.lst_hca_audioQuality = new System.Windows.Forms.ComboBox();
             this.chk_advanced = new System.Windows.Forms.CheckBox();
+            this.pnl_brstm = new System.Windows.Forms.Panel();
+            this.rb_hca_audioBitrate = new System.Windows.Forms.RadioButton();
+            this.rb_hca_audioQuality = new System.Windows.Forms.RadioButton();
+            this.lst_hca_audioQuality = new System.Windows.Forms.ComboBox();
+            this.pnl_hca = new System.Windows.Forms.Panel();
+            this.lbl_hca_conversion = new System.Windows.Forms.Label();
+            this.chk_hca_limitBitrate = new System.Windows.Forms.CheckBox();
+            this.num_hca_audioBitrate = new System.Windows.Forms.NumericUpDown();
+            this.pnl_brstm.SuspendLayout();
+            this.pnl_hca.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.num_hca_audioBitrate)).BeginInit();
             this.SuspendLayout();
             // 
             // lst_brstm_audioFormat
@@ -44,7 +53,7 @@
             "DSP-ADPCM",
             "16-bit PCM",
             "8-bit PCM"});
-            this.lst_brstm_audioFormat.Location = new System.Drawing.Point(215, 110);
+            this.lst_brstm_audioFormat.Location = new System.Drawing.Point(193, 11);
             this.lst_brstm_audioFormat.Name = "lst_brstm_audioFormat";
             this.lst_brstm_audioFormat.Size = new System.Drawing.Size(216, 39);
             this.lst_brstm_audioFormat.TabIndex = 13;
@@ -52,7 +61,7 @@
             // lbl_brstm_audioFormat
             // 
             this.lbl_brstm_audioFormat.AutoSize = true;
-            this.lbl_brstm_audioFormat.Location = new System.Drawing.Point(25, 110);
+            this.lbl_brstm_audioFormat.Location = new System.Drawing.Point(3, 11);
             this.lbl_brstm_audioFormat.Name = "lbl_brstm_audioFormat";
             this.lbl_brstm_audioFormat.Size = new System.Drawing.Size(184, 32);
             this.lbl_brstm_audioFormat.TabIndex = 14;
@@ -67,30 +76,6 @@
             this.lbl_options.TabIndex = 15;
             this.lbl_options.Text = "(extension) options:";
             // 
-            // lbl_hca_audioQuality
-            // 
-            this.lbl_hca_audioQuality.AutoSize = true;
-            this.lbl_hca_audioQuality.Location = new System.Drawing.Point(25, 110);
-            this.lbl_hca_audioQuality.Name = "lbl_hca_audioQuality";
-            this.lbl_hca_audioQuality.Size = new System.Drawing.Size(188, 32);
-            this.lbl_hca_audioQuality.TabIndex = 18;
-            this.lbl_hca_audioQuality.Text = "Audio quality:";
-            // 
-            // lst_hca_audioQuality
-            // 
-            this.lst_hca_audioQuality.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.lst_hca_audioQuality.FormattingEnabled = true;
-            this.lst_hca_audioQuality.Items.AddRange(new object[] {
-            "Highest",
-            "High",
-            "Middle",
-            "Low",
-            "Lowest"});
-            this.lst_hca_audioQuality.Location = new System.Drawing.Point(215, 110);
-            this.lst_hca_audioQuality.Name = "lst_hca_audioQuality";
-            this.lst_hca_audioQuality.Size = new System.Drawing.Size(216, 39);
-            this.lst_hca_audioQuality.TabIndex = 17;
-            // 
             // chk_advanced
             // 
             this.chk_advanced.AutoSize = true;
@@ -102,21 +87,127 @@
             this.chk_advanced.UseVisualStyleBackColor = true;
             this.chk_advanced.CheckedChanged += new System.EventHandler(this.AdvancedToggle);
             // 
+            // pnl_brstm
+            // 
+            this.pnl_brstm.Controls.Add(this.lbl_brstm_audioFormat);
+            this.pnl_brstm.Controls.Add(this.lst_brstm_audioFormat);
+            this.pnl_brstm.Location = new System.Drawing.Point(29, 110);
+            this.pnl_brstm.Name = "pnl_brstm";
+            this.pnl_brstm.Size = new System.Drawing.Size(421, 62);
+            this.pnl_brstm.TabIndex = 24;
+            // 
+            // rb_hca_audioBitrate
+            // 
+            this.rb_hca_audioBitrate.AutoSize = true;
+            this.rb_hca_audioBitrate.Location = new System.Drawing.Point(240, 12);
+            this.rb_hca_audioBitrate.Name = "rb_hca_audioBitrate";
+            this.rb_hca_audioBitrate.Size = new System.Drawing.Size(213, 36);
+            this.rb_hca_audioBitrate.TabIndex = 22;
+            this.rb_hca_audioBitrate.TabStop = true;
+            this.rb_hca_audioBitrate.Text = "Audio bitrate";
+            this.rb_hca_audioBitrate.UseVisualStyleBackColor = true;
+            this.rb_hca_audioBitrate.CheckedChanged += new System.EventHandler(this.HcaCheckboxToggle);
+            // 
+            // rb_hca_audioQuality
+            // 
+            this.rb_hca_audioQuality.AutoSize = true;
+            this.rb_hca_audioQuality.Location = new System.Drawing.Point(8, 12);
+            this.rb_hca_audioQuality.Name = "rb_hca_audioQuality";
+            this.rb_hca_audioQuality.Size = new System.Drawing.Size(217, 36);
+            this.rb_hca_audioQuality.TabIndex = 21;
+            this.rb_hca_audioQuality.Text = "Audio quality";
+            this.rb_hca_audioQuality.UseVisualStyleBackColor = true;
+            this.rb_hca_audioQuality.CheckedChanged += new System.EventHandler(this.HcaCheckboxToggle);
+            // 
+            // lst_hca_audioQuality
+            // 
+            this.lst_hca_audioQuality.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.lst_hca_audioQuality.FormattingEnabled = true;
+            this.lst_hca_audioQuality.Items.AddRange(new object[] {
+            "Highest",
+            "High",
+            "Middle",
+            "Low",
+            "Lowest"});
+            this.lst_hca_audioQuality.Location = new System.Drawing.Point(9, 67);
+            this.lst_hca_audioQuality.Name = "lst_hca_audioQuality";
+            this.lst_hca_audioQuality.Size = new System.Drawing.Size(216, 39);
+            this.lst_hca_audioQuality.TabIndex = 17;
+            this.lst_hca_audioQuality.SelectedValueChanged += new System.EventHandler(this.UpdateApproxKbps);
+            // 
+            // pnl_hca
+            // 
+            this.pnl_hca.Controls.Add(this.lbl_hca_conversion);
+            this.pnl_hca.Controls.Add(this.chk_hca_limitBitrate);
+            this.pnl_hca.Controls.Add(this.num_hca_audioBitrate);
+            this.pnl_hca.Controls.Add(this.rb_hca_audioBitrate);
+            this.pnl_hca.Controls.Add(this.rb_hca_audioQuality);
+            this.pnl_hca.Controls.Add(this.lst_hca_audioQuality);
+            this.pnl_hca.Location = new System.Drawing.Point(29, 191);
+            this.pnl_hca.Name = "pnl_hca";
+            this.pnl_hca.Size = new System.Drawing.Size(549, 220);
+            this.pnl_hca.TabIndex = 26;
+            // 
+            // lbl_hca_conversion
+            // 
+            this.lbl_hca_conversion.AutoSize = true;
+            this.lbl_hca_conversion.Location = new System.Drawing.Point(232, 68);
+            this.lbl_hca_conversion.Name = "lbl_hca_conversion";
+            this.lbl_hca_conversion.Size = new System.Drawing.Size(281, 32);
+            this.lbl_hca_conversion.TabIndex = 25;
+            this.lbl_hca_conversion.Text = "bps to Kbps / approx.";
+            // 
+            // chk_hca_limitBitrate
+            // 
+            this.chk_hca_limitBitrate.AutoSize = true;
+            this.chk_hca_limitBitrate.Location = new System.Drawing.Point(9, 125);
+            this.chk_hca_limitBitrate.Name = "chk_hca_limitBitrate";
+            this.chk_hca_limitBitrate.Size = new System.Drawing.Size(201, 36);
+            this.chk_hca_limitBitrate.TabIndex = 24;
+            this.chk_hca_limitBitrate.Text = "Limit bitrate";
+            this.chk_hca_limitBitrate.UseVisualStyleBackColor = true;
+            // 
+            // num_hca_audioBitrate
+            // 
+            this.num_hca_audioBitrate.Location = new System.Drawing.Point(9, 68);
+            this.num_hca_audioBitrate.Maximum = new decimal(new int[] {
+            999999999,
+            0,
+            0,
+            0});
+            this.num_hca_audioBitrate.Minimum = new decimal(new int[] {
+            8958,
+            0,
+            0,
+            0});
+            this.num_hca_audioBitrate.Name = "num_hca_audioBitrate";
+            this.num_hca_audioBitrate.Size = new System.Drawing.Size(216, 38);
+            this.num_hca_audioBitrate.TabIndex = 23;
+            this.num_hca_audioBitrate.Value = new decimal(new int[] {
+            320000,
+            0,
+            0,
+            0});
+            this.num_hca_audioBitrate.ValueChanged += new System.EventHandler(this.UpdateKbpsConversion);
+            // 
             // MainAdvanced
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pnl_hca);
+            this.Controls.Add(this.pnl_brstm);
             this.Controls.Add(this.chk_advanced);
-            this.Controls.Add(this.lbl_hca_audioQuality);
-            this.Controls.Add(this.lst_hca_audioQuality);
             this.Controls.Add(this.lbl_options);
-            this.Controls.Add(this.lbl_brstm_audioFormat);
-            this.Controls.Add(this.lst_brstm_audioFormat);
             this.Name = "MainAdvanced";
             this.Text = "VGAudio";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.OnClose);
             this.Load += new System.EventHandler(this.OnLoad);
+            this.pnl_brstm.ResumeLayout(false);
+            this.pnl_brstm.PerformLayout();
+            this.pnl_hca.ResumeLayout(false);
+            this.pnl_hca.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.num_hca_audioBitrate)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -127,8 +218,14 @@
         private System.Windows.Forms.ComboBox lst_brstm_audioFormat;
         private System.Windows.Forms.Label lbl_brstm_audioFormat;
         private System.Windows.Forms.Label lbl_options;
-        private System.Windows.Forms.Label lbl_hca_audioQuality;
-        private System.Windows.Forms.ComboBox lst_hca_audioQuality;
         private System.Windows.Forms.CheckBox chk_advanced;
+        private System.Windows.Forms.Panel pnl_brstm;
+        private System.Windows.Forms.RadioButton rb_hca_audioBitrate;
+        private System.Windows.Forms.RadioButton rb_hca_audioQuality;
+        private System.Windows.Forms.ComboBox lst_hca_audioQuality;
+        private System.Windows.Forms.Panel pnl_hca;
+        private System.Windows.Forms.NumericUpDown num_hca_audioBitrate;
+        private System.Windows.Forms.CheckBox chk_hca_limitBitrate;
+        private System.Windows.Forms.Label lbl_hca_conversion;
     }
 }
--- MainAdvanced.cs
+++ MainAdvanced.cs
@@ -14,10 +14,17 @@ namespace VGAudio.Win32
     public partial class MainAdvanced : Form
     {
         private readonly string exportExtension;
+        private readonly Dictionary<string, int> BitrateValue = new Dictionary<string, int>();
         public MainAdvanced(string exportExtension)
         {
             InitializeComponent();
             this.exportExtension = exportExtension.ToLower();
+
+            BitrateValue.Add("Highest", 353);
+            BitrateValue.Add("High", 235);
+            BitrateValue.Add("Middle", 176);
+            BitrateValue.Add("Low", 117);
+            BitrateValue.Add("Lowest", 88);
         }
 
         private void OnLoad(object sender, EventArgs e)
@@ -30,6 +37,7 @@ namespace VGAudio.Win32
             // Load if the advanced settings should be applied
             UpdateValuesFromFields();
             AdvancedToggle();
+            HcaCheckboxToggle();
         }
 
         private void UpdateValuesFromFields()
@@ -37,6 +45,31 @@ namespace VGAudio.Win32
             chk_advanced.Checked = (bool)Main.AdvancedSettings["Apply"];
             var brstm_audioFormat = (string)Main.AdvancedSettings["BRSTM_audioFormat"];
             var hca_audioQuality = (string)Main.AdvancedSettings["HCA_audioQuality"];
+
+            decimal hca_audioBitrate;
+            if (Main.AdvancedSettings["HCA_audioBitrate"] != null)
+            {
+                hca_audioBitrate = (decimal)Main.AdvancedSettings["HCA_audioBitrate"];
+                if (hca_audioBitrate < num_hca_audioBitrate.Minimum || hca_audioBitrate > num_hca_audioBitrate.Maximum)
+                {
+                    // Set the default bitrate value from the form
+                    hca_audioBitrate = (int)num_hca_audioBitrate.Value;
+                }
+            }
+            else
+            {
+                hca_audioBitrate = num_hca_audioBitrate.Value;
+            }
+
+            bool hca_limitBitrate;
+            if (Main.AdvancedSettings["HCA_limitBitrate"] != null)
+            {
+                hca_limitBitrate = (bool)Main.AdvancedSettings["HCA_limitBitrate"];
+            }
+            else
+            {
+                hca_limitBitrate = false;
+            }
 
             if (brstm_audioFormat != null)
             {
@@ -57,6 +90,23 @@ namespace VGAudio.Win32
                 // Default value: High
                 lst_hca_audioQuality.SelectedIndex = 1;
             }
+
+            num_hca_audioBitrate.Value = hca_audioBitrate;
+            chk_hca_limitBitrate.Checked = hca_limitBitrate;
+
+            switch (Main.AdvancedSettings["HCA_audioRadioButtonSelector"])
+            {
+                case "bitrate":
+                    rb_hca_audioQuality.Checked = false;
+                    rb_hca_audioBitrate.Checked = true;
+                    break;
+                case "quality":
+                case null:
+                default:
+                    rb_hca_audioQuality.Checked = true;
+                    rb_hca_audioBitrate.Checked = false;
+                    break;
+            }
         }
 
         private void AdvancedToggle(object sender = null, EventArgs e = null)
@@ -70,34 +120,45 @@ namespace VGAudio.Win32
                 switch (exportExtension)
                 {
                     case "brstm":
-                        lbl_brstm_audioFormat.Visible = true;
-                        lst_brstm_audioFormat.Visible = true;
-                        lbl_hca_audioQuality.Visible = false;
-                        lst_hca_audioQuality.Visible = false;
+                        pnl_brstm.Visible = true;
+                        pnl_hca.Visible = false;
                         break;
                     case "hca":
-                        lbl_brstm_audioFormat.Visible = false;
-                        lst_brstm_audioFormat.Visible = false;
-                        lbl_hca_audioQuality.Visible = true;
-                        lst_hca_audioQuality.Visible = true;
+                        pnl_brstm.Visible = false;
+                        pnl_hca.Visible = true;
                         break;
                     default:
                         Main.AdvancedSettings["Apply"] = false;
-                        lbl_brstm_audioFormat.Visible = false;
-                        lst_brstm_audioFormat.Visible = false;
-                        lbl_hca_audioQuality.Visible = false;
-                        lst_hca_audioQuality.Visible = false;
+                        pnl_brstm.Visible = false;
+                        pnl_hca.Visible = false;
                         break;
                 }
             }
             else
             {
-                lbl_brstm_audioFormat.Visible = false;
-                lst_brstm_audioFormat.Visible = false;
-                lbl_hca_audioQuality.Visible = false;
-                lst_hca_audioQuality.Visible = false;
+                pnl_brstm.Visible = false;
+                pnl_hca.Visible = false;
             }
             lbl_options.Visible = (bool)Main.AdvancedSettings["Apply"];
+        }
+
+        private void HcaCheckboxToggle(object sender = null, EventArgs e = null)
+        {
+            lst_hca_audioQuality.Visible = rb_hca_audioQuality.Checked;
+            num_hca_audioBitrate.Visible = rb_hca_audioBitrate.Checked;
+
+            if (rb_hca_audioQuality.Checked) UpdateApproxKbps();
+            if (rb_hca_audioBitrate.Checked) UpdateKbpsConversion();
+        }
+
+        private void UpdateKbpsConversion(object sender = null, EventArgs e = null)
+        {
+            lbl_hca_conversion.Text = String.Format("bps = {0} Kbps", num_hca_audioBitrate.Value / 1000);
+        }
+        private void UpdateApproxKbps(object sender = null, EventArgs e = null)
+        {
+            int approxValue = BitrateValue[lst_hca_audioQuality.SelectedItem.ToString()];
+            lbl_hca_conversion.Text = String.Format("= approx. {0} Kbps", approxValue);
         }
 
         private void OnClose(object sender, FormClosedEventArgs e)
@@ -108,8 +169,27 @@ namespace VGAudio.Win32
 
         private void Apply()
         {
-            Main.AdvancedSettings["BRSTM_audioFormat"] = lst_brstm_audioFormat.SelectedItem.ToString();
-            Main.AdvancedSettings["HCA_audioQuality"] = lst_hca_audioQuality.SelectedItem.ToString();
+            switch (exportExtension)
+            {
+                case "brstm":
+                    Main.AdvancedSettings["BRSTM_audioFormat"] = lst_brstm_audioFormat.SelectedItem.ToString();
+                    break;
+                case "hca":
+                    if (rb_hca_audioQuality.Checked)
+                    {
+                        Main.AdvancedSettings["HCA_audioQuality"] = lst_hca_audioQuality.SelectedItem.ToString();
+                    }
+                    if (rb_hca_audioBitrate.Checked) Main.AdvancedSettings["HCA_audioBitrate"] = num_hca_audioBitrate.Value;
+                    Main.AdvancedSettings["HCA_limitBitrate"] = chk_hca_limitBitrate.Checked;
+
+                    if (rb_hca_audioBitrate.Checked) Main.AdvancedSettings["HCA_audioRadioButtonSelector"] = "bitrate";
+                    if (rb_hca_audioQuality.Checked) Main.AdvancedSettings["HCA_audioRadioButtonSelector"] = "quality";
+                    break;
+                default:
+                    Main.AdvancedSettings["Apply"] = false;
+                    return;
+            }
+            Main.AdvancedSettings["Apply"] = chk_advanced.Checked;
         }
 
         public static void Reset()
@@ -117,7 +197,10 @@ namespace VGAudio.Win32
             Main.AdvancedSettings.Clear();
             Main.AdvancedSettings.Add("Apply", false);
             Main.AdvancedSettings.Add("BRSTM_audioFormat", null);
+            Main.AdvancedSettings.Add("HCA_audioRadioButtonSelector", null);
             Main.AdvancedSettings.Add("HCA_audioQuality", null);
+            Main.AdvancedSettings.Add("HCA_audioBitrate", null);
+            Main.AdvancedSettings.Add("HCA_limitBitrate", null);
         }
     }
 }
