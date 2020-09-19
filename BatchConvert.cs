3fe07dfa3bddb75278accc9a4e425892f071a078
DavidL344 <69001314+DavidL344@users.noreply.github.com>
2020-09-19 20:48:46 +0200

    Add "batch conversion" section

---
+++ BatchConvert.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VGAudio.Win32
+{
+    public partial class BatchConvert : Form
+    {
+        private string[] files;
+        public BatchConvert(string[] files)
+        {
+            InitializeComponent();
+            this.files = files;
+        }
+
+        private void OnLoad(object sender, EventArgs e)
+        {
+            MinimumSize = Size;
+            MaximumSize = Size;
+            MaximizeBox = false;
+        }
+    }
+}
